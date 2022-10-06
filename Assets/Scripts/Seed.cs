c8586237e94dbca8fa7ad68a9042ce04ce2fda0e
Rumaisa Abdulhai <rumaisa.abdulhai@gmail.com>
2022-10-06 18:34:39 -0400

    seed hits soil

--- Assets/Scripts/Seed.cs
+++ Assets/Scripts/Seed.cs
@@ -3,7 +3,6 @@ using UnityEngine.Events;
 
 public class Seed : MonoBehaviour
 {
-    private bool hitSoil = false; // if the seed has hit soil
     public LayerMask soilLayer; // all soils
     public Transform plantLocation;  // Point to check plant range from
     public float plantRange;  // How far can the player reach to plant a seed
@@ -13,10 +12,15 @@ public class Seed : MonoBehaviour
         
         if (gameObject.GetComponent<Collider2D>().IsTouchingLayers(soilLayer))
         {
-            Debug.Log("Hit Soil!");
-            // Soil soil = hitSoils[i].gameObject.GetComponent<Soil>();
-            // soil.PlantSeed();
-            Destroy(gameObject);            
+            
+            Collider2D[] hitSoils = Physics2D.OverlapCircleAll(plantLocation.position, plantRange, soilLayer);
+            for (int i = 0; i < hitSoils.Length; i++)
+            {
+                Soil soil = hitSoils[i].gameObject.GetComponent<Soil>(); 
+                soil.PlantSeed();
+                Destroy(gameObject);
+            }
+            
         }
 
     }
