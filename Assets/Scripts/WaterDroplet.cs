1ad2dc007d243f2e9ac43c803bb361a7b7ad8821
Rumaisa Abdulhai <rumaisa.abdulhai@gmail.com>
2022-10-12 22:45:18 -0400

    change gitignore and add initial script for water droplet

---
+++ Assets/Scripts/WaterDroplet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaterDroplet : MonoBehaviour
+{
+    // All soils
+    public LayerMask soilLayer;
+
+    // Ground layer
+    public LayerMask groundLayer;
+
+    // Point to check watering range from
+    public Transform waterLocation;
+
+    // How far can the player reach to water soil
+    public float waterRange;
+
+    void Update()
+    {
+        // TODO this should only run while held by player
+
+        if (GetComponent<Collider2D>().IsTouchingLayers(soilLayer))
+        {
+            Collider2D[] hitSoils = Physics2D.OverlapCircleAll(waterLocation.position, waterRange, soilLayer);
+            for (int i = 0; i < hitSoils.Length; i++)
+            {
+                Soil soil = hitSoils[i].gameObject.GetComponent<Soil>();
+                soil.WaterSoil();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
