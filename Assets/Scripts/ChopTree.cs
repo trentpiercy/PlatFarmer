492598ce5fb9a10a4f709707986f82968b2ff7ca
Trent Piercy <trentjamespiercy@gmail.com>
2022-10-18 20:54:37 -0400

    chop animation

--- Assets/Scripts/ChopTree.cs
+++ Assets/Scripts/ChopTree.cs
@@ -12,15 +12,32 @@ public class ChopTree : MonoBehaviour
     // How far can the player reach to chop a tree
     public float chopRange;
 
+    private bool isChopping = false;
+    private float chopDegrees = 60;
+    private float delay = 0.2f;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)){
+        if (Input.GetKeyDown(KeyCode.E) && !isChopping){
+
+            isChopping = true;
+            transform.Rotate(new Vector3(0, 0, -chopDegrees));
 
             Collider2D[] hitTrees = Physics2D.OverlapCircleAll(chopLocation.position, chopRange, treesLayer);
             for (int i = 0; i < hitTrees.Length; i++)
             {
                 hitTrees[i].gameObject.GetComponent<TreeFall>().FallDown();
             }
+
+            StartCoroutine(ChopWait());
         }
+    }
+
+    IEnumerator ChopWait()
+    {
+        yield return new WaitForSeconds(delay);
+
+        transform.Rotate(new Vector3(0, 0, chopDegrees));
+        isChopping = false;
     }
 }
