083c41525b4faac54538c090b5014fafcd76161c
Trent Piercy <trentjamespiercy@gmail.com>
2022-10-19 21:31:57 -0400

    improve item pickup/put down

--- Assets/Scripts/HoldObject.cs
+++ Assets/Scripts/HoldObject.cs
@@ -6,88 +6,52 @@ public class HoldObject : MonoBehaviour
     // Reference to where you want your object to go
     public GameObject playerHands;
 
-    public int itemLayer = 10;
-
-    // The gameobject you collided with
-    private GameObject collidedWith;
-    private bool canpickup = false;
-    private bool hasItem = false;
+    private GameObject heldItem;
+    public LayerMask itemLayerMask;
 
 
     void Update()
     {
-        if (canpickup && !hasItem && Input.GetKeyDown(KeyCode.F)) // if you enter the collider of the object
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Assert(collidedWith != null);
-
-            Debug.Log("Picked up!");
-            
-            if (collidedWith.CompareTag("Water"))
+            if (heldItem == null)
             {
-                Debug.Log("Picking up water");
-                // If collided with water, hold the parent object
-                collidedWith = collidedWith.transform.parent.gameObject;
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                if (GetComponent<Collider2D>().IsTouchingLayers(itemLayerMask))
+                {
+                    Debug.Log("Touching item");
+                    Collider2D[] items = Physics2D.OverlapCircleAll(transform.position, 0.5f, itemLayerMask);
+
+                    if (items.Length == 0) return;
+
+                    // Pickup the first item
+                    heldItem = items[0].gameObject;
+                    heldItem = heldItem.transform.parent.gameObject;
+                    heldItem.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+
+                    if (heldItem.CompareTag("Axe"))
+                    {
+                        heldItem.GetComponent<ChopTree>().enabled = true;
+                    }
+
+                    heldItem.transform.position = playerHands.transform.position; // sets the position of the object to your hand position
+                    heldItem.transform.parent = playerHands.transform; // makes the object become a child of the parent so that it moves with the hands
+                }
             }
-            else if (collidedWith.CompareTag("Seed"))
+            else // drop
             {
-                Debug.Log("Picking up seed");
-                // If collided with seed, hold the parent object
-                collidedWith = collidedWith.transform.parent.gameObject;
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-            } else if (collidedWith.CompareTag("Axe"))
-            {
-                Debug.Log("Picking up axe");
-                collidedWith = collidedWith.transform.parent.gameObject;
+                Debug.Assert(heldItem != null);
+                Debug.Log("Put down!");
 
-                collidedWith.GetComponent<ChopTree>().enabled = true;
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                heldItem.transform.parent = null; // make the object not be a child of the hands
+
+                heldItem.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                if (heldItem.CompareTag("Axe"))
+                {
+                    heldItem.GetComponent<ChopTree>().enabled = false;
+                }
+
+                heldItem = null;
             }
-
-            collidedWith.transform.position = playerHands.transform.position; // sets the position of the object to your hand position
-            collidedWith.transform.parent = playerHands.transform; // makes the object become a child of the parent so that it moves with the hands
-
-            hasItem = true;
         }
-        else if (hasItem && Input.GetKeyDown(KeyCode.F)) // drop
-        {
-            Debug.Assert(collidedWith != null);
-
-            Debug.Log("Put down!");
-
-            collidedWith.transform.parent = null; // make the object not be a child of the hands
-            
-            if (collidedWith.CompareTag("Axe")){
-                collidedWith.GetComponent<ChopTree>().enabled = false;
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            } else if (collidedWith.CompareTag("Seed"))
-            {
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            }
-            else if (collidedWith.CompareTag("Water"))
-            {
-                collidedWith.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            }
-
-            hasItem = false;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Entered trigger, layer: " + other.gameObject.layer);
-        if (hasItem) return;
-        if (other.gameObject.layer == itemLayer) // Item layer
-        {
-            Debug.Log("Trigger is item layer");
-            canpickup = true;
-            collidedWith = other.gameObject;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        //Debug.Log("Exit trigger");
-        canpickup = false;
     }
 }
