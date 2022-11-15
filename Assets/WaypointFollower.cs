9347353f1320cfa032a566768ebdfd14f37d5450
Rumaisa Abdulhai <rumaisa.abdulhai@gmail.com>
2022-11-15 16:17:47 -0500

    changes to waypoint follower

--- Assets/WaypointFollower.cs
+++ Assets/WaypointFollower.cs
@@ -10,6 +10,8 @@ public class WaypointFollower : MonoBehaviour
 
     [SerializeField] private float speed = 2f;
 
+    public GameObject beaver;
+
     // Update is called once per frame
     private void Update()
     {
@@ -17,11 +19,14 @@ public class WaypointFollower : MonoBehaviour
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
         {
             currentWaypointIndex++;
+            beaver.transform.Rotate(0, 180, 0);
             if (currentWaypointIndex >= waypoints.Length)
             {
                 currentWaypointIndex = 0;
             }
         }
+        
+        
 
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position,
             Time.deltaTime * speed);
