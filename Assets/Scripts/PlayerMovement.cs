73afefcf1a0f956538c3d9fdd8c92d91fe0cd9cc
Trent Piercy <trentjamespiercy@gmail.com>
2022-11-22 16:42:31 -0500

    move gems

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,7 +22,6 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(Input.GetAxisRaw("Horizontal"));
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 		if (Mathf.Abs(horizontalMove) > 0)
 		{
