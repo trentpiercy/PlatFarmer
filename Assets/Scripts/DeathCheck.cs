deaee15c06f67b8006db70e2f042c4b6ae464e8a
19mileshudgins <hudginsmiles@gmail.com>
2022-11-29 17:36:34 -0500

    fixed merge issue

--- Assets/Scripts/DeathCheck.cs
+++ Assets/Scripts/DeathCheck.cs
@@ -37,10 +37,8 @@ public class DeathCheck : MonoBehaviour
 
     private IEnumerator OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == enemyLayer || 
-            other.gameObject.layer == waterLayer)
+        if (other.gameObject.layer == enemyLayer)
         {
-            Debug.Log("On Water");
             // TODO this is not epic code
             other.transform.parent.GetComponent<Enemy>().Hit(transform);
             loseLife();
